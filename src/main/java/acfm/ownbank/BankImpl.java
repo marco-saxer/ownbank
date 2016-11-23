cf6df6a3552dd4f8f7ae2eead629159cd45f73c1
Schweigert <conny.schweigert@accenture.com>
2016-11-23 12:23:07 +0100

    Commited NumberGenerator Bank createAccount

---
+++ src/main/java/acfm/ownbank/BankImpl.java
@@ -0,0 +1,46 @@
+package acfm.ownbank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankImpl implements Bank {
+
+	AccountNumberGenerator generator;
+	
+	Map accounts = new HashMap();
+	
+	public BankImpl(AccountNumberGenerator generator) {
+		super();
+		this.generator = generator;
+	}
+
+	public int createAccount() {
+		// TODO Auto-generated method stub
+		Account accountNew = new AccountImpl(generator);	
+		int accNumber = accountNew.getAcountNumber();
+		accounts.put(accNumber,accountNew);
+		return accNumber;
+	}
+
+	public void deposit(int accNumber, int amount) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	public void transfer(int accNumberFrom, int accNumberTo, int amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void withdrawal(int accNumber, int amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getBalance(int accNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
