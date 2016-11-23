2af4a10a9112e70cbffdd150bea5362946544016
marco.saxer <marco.saxer@CPX-XA89L2HA6H6.dir.svc.accenture.com>
2016-11-23 17:15:20 +0100

    updated

--- src/main/java/acfm/ownbank/AccountFactoryImpl.java
+++ src/main/java/acfm/ownbank/AccountFactoryImpl.java
@@ -1,26 +1,26 @@
-// package acfm.ownbank;
-//
-// public class AccountFactoryImpl implements AccountFactory {
-//
-// private AccountNumberGenerator accountNumberGenerator;
-//
-// public AccountFactoryImpl() {
-// accountNumberGenerator = new AccountNumberGeneratorImpl();
-// }
-//
-//	public AccountReadable createAccount(int creditLine, AccountType accountType) {
-//		AbstractAccount newAccount = null;
-//		int accNumber = accountNumberGenerator.getAccountNumber();
-//		switch (accountType) {
-//		case CREDIT_ACCOUNT:
-//			newAccount = new CreditAccount(accNumber, creditLine);
-//			break;
-//		case SAVING_ACCOUNT:
-//			newAccount = new SavingAccount(accNumber);
-//			break;
-//		}
-//
-//		return newAccount;
-//	}
-//
-// }
+package acfm.ownbank;
+
+public class AccountFactoryImpl implements AccountFactory {
+
+	private AccountNumberGenerator accountNumberGenerator;
+
+	public AccountFactoryImpl() {
+		accountNumberGenerator = new AccountNumberGeneratorImpl();
+	}
+
+	public AccountReadable createAccount(int creditLine, AccountType accountType) {
+		AbstractAccount newAccount = null;
+		int accNumber = accountNumberGenerator.getAccountNumber();
+		switch (accountType) {
+		case CREDIT_ACCOUNT:
+			newAccount = new CreditAccount(accNumber, creditLine);
+			break;
+		case SAVING_ACCOUNT:
+			newAccount = new SavingAccount(accNumber);
+			break;
+		}
+
+		return newAccount;
+	}
+
+}
